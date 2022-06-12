5640cded5c38cb93e54ac382fec8b4abf3950a2c
Jimmy <jamesmwoolley@gmail.com>
2022-06-11 20:26:32 -0600

    moreg

--- lib/Services/TreeFactory.cs
+++ lib/Services/TreeFactory.cs
@@ -5,20 +5,13 @@ namespace lib.Services;
 
 public static class TreeExtensions
 {
-	public static Dictionary<int, List<TreeNode>> GetUplineToChildren(this IEnumerable<TreeNode> nodes)
-	{
-		return nodes
-			.GroupBy(a => a.UplineAssociateId)
-			.ToDictionary(a => a.Key, a => a.ToList());
-	}
-
 	public static List<TreeNode> GetChildren(this Dictionary<int, List<TreeNode>> nodeToChildren, TreeNode node, HashSet<TreeNode>? alreadyVisited = null)
 	{
-		var children = nodeToChildren.ContainsKey(node.AssociateId)
+		List<TreeNode> children = nodeToChildren.ContainsKey(node.AssociateId)
 			? nodeToChildren[node.AssociateId]
 			: Array.Empty<TreeNode>().ToList();
 
-		if (alreadyVisited != null)
+		if (alreadyVisited != null && children.Any())
 		{
 			children = children.Where(a => !alreadyVisited.Contains(a)).ToList();
 		}
@@ -31,53 +24,80 @@ public class TreeFactory
 {
 	public static Tree BuildTree(IEnumerable<TreeNode> nodes)
 	{
+		if (nodes == null || !nodes.Any())
+		{
+			throw new InvalidTreeException("nodes is null or empty");
+		}
+
 		Dictionary<int, TreeNode> idToNode = nodes.ToDictionary(a => a.AssociateId);
 
-		Dictionary<int, List<TreeNode>> uplineToChildren = nodes.GetUplineToChildren();
+		Dictionary<int, List<TreeNode>> uplineToChildren = nodes
+			.GroupBy(a => a.UplineAssociateId)
+			.ToDictionary(a => a.Key, a => a.ToList());
 
-		TreeNode root = nodes.Single(a => a.UplineAssociateId == 0);
+		List<TreeNode> roots = nodes
+			.Where(a => a.UplineAssociateId == 0)
+			.ToList();
+
+		if (!roots.Any())
+		{
+			throw new InvalidTreeException("no root found");
+		}
+
+		if (roots.Count > 1)
+		{
+			throw new InvalidTreeException("more than one root");
+		}
+
+		TreeNode root = roots.Single();
 
 		BuildTree_Iterative(root, uplineToChildren);
 
 		return new Tree(root, idToNode);
 	}
 
-	public static void BuildTree_Recurse(TreeNode node, Dictionary<int, List<TreeNode>> uplineToChildren, HashSet<int> alreadyTraversed)
+	public static void BuildTree_Recurse(TreeNode node, Dictionary<int, List<TreeNode>> uplineToChildren, HashSet<TreeNode> alreadyTraversed)
 	{
-		List<TreeNode> children = uplineToChildren.ContainsKey(node.AssociateId)
-			? uplineToChildren[node.AssociateId]
-			: Array.Empty<TreeNode>().ToList();
+		if (node == null)
+		{
+			return;
+		}
 
-		alreadyTraversed.Add(node.AssociateId);
+		List<TreeNode> children = uplineToChildren.GetChildren(node, alreadyTraversed);
 
-		foreach (TreeNode child in children.Where(c => !alreadyTraversed.Contains(c.AssociateId)))
+		foreach (TreeNode child in children)
 		{
 			BuildTree_Recurse(child, uplineToChildren, alreadyTraversed);
 			child.UplineAssociateId = node.AssociateId;
 			child.UplineTreeNode = node;
 		}
 
+		alreadyTraversed.Add(node);
+
 		node.ChildTreeNodes = children;
 	}
 
-	public static void BuildTree_Iterative(TreeNode root, Dictionary<int, List<TreeNode>> uplineToChildren)
+	public static void BuildTree_Iterative(TreeNode? root, Dictionary<int, List<TreeNode>> uplineToChildren)
 	{
+		if (root == null)
+		{
+			return;
+		}
+
 		Stack<TreeNode> traversal = new();
 		traversal.Push(root);
 
 		Stack<TreeNode> order = new();
 		HashSet<TreeNode> alreadyVisited = new();
 
-		List<TreeNode> GetChildren(TreeNode node) => uplineToChildren.ContainsKey(node.AssociateId)
-				? uplineToChildren[node.AssociateId]
-				: Array.Empty<TreeNode>().ToList();
-
 		while (traversal.Any())
 		{
 			TreeNode node = traversal.Pop();
 			order.Push(node);
 
-			foreach  (TreeNode child in GetChildren(node))
+			alreadyVisited.Add(node);
+
+			foreach  (TreeNode child in uplineToChildren.GetChildren(node, alreadyVisited))
 			{
 				traversal.Push(child);
 			}
@@ -87,7 +107,7 @@ public class TreeFactory
 		{
 			TreeNode node = order.Pop();
 
-			List<TreeNode> children = GetChildren(node);
+			List<TreeNode> children = uplineToChildren.GetChildren(node);
 
 			foreach (TreeNode child in children)
 			{
